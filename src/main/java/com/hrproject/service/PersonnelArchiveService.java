27bead8a6ddb9a1e0388351459d90503e352a816
Zuoshihao <1352829101@qq.com>
2020-11-24 09:05:09 +0800

    添加页面

--- src/main/java/com/hrproject/service/PersonnelArchiveService.java
+++ src/main/java/com/hrproject/service/PersonnelArchiveService.java
@@ -1,10 +1,10 @@
-package com.hrproject.dao;
+package com.hrproject.service;
 
 import com.hrproject.entity.PersonnelArchive;
 
 import java.util.List;
 
-public interface PersonnelArchiveDao {
+public interface PersonnelArchiveService {
 
     //分页及动态查询人事档案信息
     List<PersonnelArchive> findPersonnelArchive(PersonnelArchive personnelArchive);
