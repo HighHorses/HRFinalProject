ba1169f6d9455257c343303358ebd77b0d4550a4
angleEye66 <826868245@qq.com>
2020-11-24 14:23:45 +0800

    公共类

---
+++ src/main/java/com/hrproject/util/Constants.java
@@ -0,0 +1,11 @@
+package com.hrproject.util;
+
+public class Constants {
+    public final static String FRRDBACK1= "优";
+    public final static String FRRDBACK2= "良";
+    public final static String FRRDBACK3= "差";
+    public final static String STATUS1= "起草";
+    public final static String STATUS2= "审核中";
+    public final static String STATUS3= "审核通过";
+    public final static String STATUS4= "驳回";
+}
